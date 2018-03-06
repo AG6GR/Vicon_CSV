bd4a81e45f412ad4e35b3526167c19562ef92dca
AG6GR <slhe@princeton.edu>
2018-03-05 23:40:37 -0500

    Bump minor version number

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ from math import degrees
 bl_info = {
     "name": "Vicon CSV",
     "author": "Sunny He",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (2, 79, 0),
     "location": "File > Import/Export > Vicon CSV (.csv)",
     "description": "Import Vicon tracking data as object animation",
